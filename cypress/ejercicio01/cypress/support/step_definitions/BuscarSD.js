7282521564177ef0bcf17854931213125cc1b050
carlos guzman <carlosguzmanbaquero@gmail.com>
2021-03-20 11:46:01 -0500

    continuacion primer ejemplo cypress, integrando jerkin y cucumber, forma opcionar creando una clase

--- cypress/ejercicio01/cypress/support/step_definitions/BuscarSD.js
+++ cypress/ejercicio01/cypress/support/step_definitions/BuscarSD.js
@@ -1,18 +1,18 @@
 import { Given, When, Then} from "cypress-cucumber-preprocessor/steps";
-import { navegar, buscarTitulo, irAlArticulo, buscarTexto }  from '../../Page/BuscarPage';
+import { BuscarPage }  from '../../Page/BuscarPage';
 
 Given('Navegar en la pagina del blog', () =>{
-    navegar('');
+    BuscarPage.prototype.navegar();
 })
 
 When ('Busco un articulo', () =>{
-    buscarTitulo('Variables en GO');
+    BuscarPage.prototype.buscarTitulo('Variables en GO');
 })
 
 Then ('Voy al articulo', () =>{
-    irAlArticulo('Variables en GO');
+    BuscarPage.prototype.irAlArticulo('Variables en GO');
 })
 
 When ('Busco una palabra', () =>{
-    buscarTexto('var nombre tipo = expresión');
+    BuscarPage.prototype.buscarTexto('var nombre tipo = expresión');
 })
